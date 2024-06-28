ee700be7c20672587f18a857f4e80c2478c09cd9
Naden <naden@mac.com>
2024-06-28 11:51:33 +1000

    Update

--- app/services/s3.rb
+++ app/services/s3.rb
@@ -1,13 +1,15 @@
 require "aws-sdk-s3"
 
 class S3
-  @credentials = Aws::Credentials.new(ENV.fetch("harana_aws_access_key", nil), ENV.fetch("harana_aws_secret_key", nil))
-  @s3 = Aws::S3::Resource.new(region: "ap-southeast-2", credentials: @credentials)
-  @bucket = @s3.bucket("harana-website-haranadev")
 
   # Save a string as an object to the specified S3 bucket
   def save_object(key, content, overwrite: true)
-    obj = @bucket.object(key)
+
+    credentials = Aws::Credentials.new(ENV.fetch("harana_aws_access_key", nil), ENV.fetch("harana_aws_secret_key", nil))
+    s3 = Aws::S3::Resource.new(region: "ap-southeast-2", credentials: @credentials)
+    bucket = s3.bucket("harana-website-haranadev")
+  
+    obj = bucket.object(key)
     if overwrite
       obj.put(body: content)
       puts "Content uploaded to #{obj.public_url}"
@@ -24,16 +26,16 @@ class S3
     puts "Failed to upload content: #{e.message}"
   end
 
-  # Delete an object from the specified S3 bucket
-  def delete_s3_object(key)
-    obj = @bucket.object(key)
-    if obj.delete
-      puts "File #{key} deleted successfully."
-    else
-      puts "Failed to delete file."
-    end
-  rescue StandardError => e
-    puts "Failed to delete file: #{e.message}"
-  end
+  # # Delete an object from the specified S3 bucket
+  # def delete_s3_object(key)
+  #   obj = @bucket.object(key)
+  #   if obj.delete
+  #     puts "File #{key} deleted successfully."
+  #   else
+  #     puts "Failed to delete file."
+  #   end
+  # rescue StandardError => e
+  #   puts "Failed to delete file: #{e.message}"
+  # end
 
 end
