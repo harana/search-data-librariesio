5a7929f2b30485cf1de514491183d967e6b06381
Naden <naden@mac.com>
2024-06-28 12:09:21 +1000

    Update

--- app/services/s3.rb
+++ app/services/s3.rb
@@ -18,7 +18,6 @@ class S3
 
     obj = bucket.object(key)
     if overwrite
-      delete_s3_object(key)
       obj.upload_file(file_name)
       puts "Content uploaded to #{obj.public_url}"
     else
